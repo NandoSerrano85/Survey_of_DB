1c094d58b6524b6bdbf400a4c247bc8db9383853
Fernando Serrano <fserr009@fiu.edu>
2018-03-20 19:23:04 -0400

    corrected issues with query but still displaying doubles

--- assignment3/q1.php
+++ assignment3/q1.php
@@ -7,14 +7,14 @@ $visited = isset($_POST['visited']) ? $_POST['visited'] : '';
 $errormsg = '';
 
 if (!($dependents )) {
-  if ($visited) {     
+  if ($visited) {
      $errormsg = 'Please enter a dependent name';
   }
 
  // printing the form to enter the user input
  print <<<_HTML_
  <FORM method="POST" action="{$_SERVER['PHP_SELF']}">
- &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 
+ &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
  <font color= 'red'>$errormsg</font><br>
  Department Number: <input type="text" name="dependents" size="9" value="$dependents">
  <br/>
@@ -23,27 +23,51 @@ if (!($dependents )) {
  <INPUT type="hidden" name="visited" value="true">
  </FORM>
 _HTML_;
- 
+
 }
 else {
-  require ('./dbConfig.php');
-  $querystring = "SELECT e.fname, e.lname, m.fname, m.lname de.dependent_name FROM employee as e,  employee as m, deparment as d, dependent as de  where de.dependent_name = $dependents and de.essn = e.ssn and e.dno = d.dnumber and d.mgr_ssn = m.snn";
-  $result = mysqli_query($con, $querystring);
-  if (!$result) {
-    print ( "Could not successfully run query ($querystring) from DB: " . mysqli_error($con) . "<br>");
+  $host = "localhost";
+  $user="root";
+  $password="";
+  $dbname = "company";
+  $con=mysqli_connect($host, $user, $password, $dbname);
+  if (mysqli_connect_errno()) {
+    echo "Failed to connect to MariaDB: " . mysqli_connect_error();
+    exit;
+  }
+  $querystring1 = "
+      SELECT e.fname, e.lname, de.dependent_name
+      FROM employee e,  employee m, department d, dependent de
+      WHERE de.dependent_name = '$dependents' AND de.essn = e.ssn
+      AND e.dno = d.dnumber AND d.mgrssn = m.ssn";
+
+  $querystring2 = "
+          SELECT m.fname, m.lname
+          FROM employee e,  employee m, department d, dependent de
+          WHERE de.dependent_name = '$dependents' AND de.essn = e.ssn
+          AND e.dno = d.dnumber AND d.mgrssn = m.ssn";
+  $dep_emp = mysqli_query($con, $querystring1);
+  $emp_mgr = mysqli_query($con, $querystring2);
+  if (!$dep_emp) {
+    print ( "Could not successfully run query ($querystring1) from DB: " . mysqli_error($con) . "<br>");
     exit;
   }
 
-  if (mysqli_num_rows($result) == 0) {
+  if (!$emp_mgr) {
+    print ( "Could not successfully run query ($querystring2) from DB: " . mysqli_error($con) . "<br>");
+    exit;
+  }
+  if (mysqli_num_rows($emp_mgr) == 0 && mysqli_num_rows($dep_emp) == 0) {
     print ("No rows found, nothing to print so am exiting<br>");
     exit;
   }
 
   print("Results: <br>");
-  while ($rows = mysqli_fetch_assoc($result)) {
-    foreach ($rows as $row) {
-      print $row." ";
-    }
+  while (($q1 = mysqli_fetch_assoc($dep_emp)) && ($q2 = mysqli_fetch_assoc($emp_mgr))) {
+      print("Dependent: $q1[dependent_name] <br>");
+      print("Employee: $q1[fname] $q1[lname]<br>");
+      print("Manager: $q2[fname] $q2[lname]<br>");
+
     print "<br>";
   }
   mysqli_close($con);
